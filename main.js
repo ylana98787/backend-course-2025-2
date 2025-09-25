8165bf9b2289049c12838dfb6e2acff9522e31ce
Ulana <ylana98787@gmail.com>
2025-09-25 21:15:24 +0300

    Completed lab2: URLs, curl request, Node.js program

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+const { URL, URLSearchParams } = require('url');
+const date = "20231001";       
+const time_period = "quarter"; 
+const baseUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/bankfin");
+baseUrl.search = new URLSearchParams({
+    date: date,
+    period: time_period,
+    json: "" 
+});
+console.log(baseUrl.toString());
